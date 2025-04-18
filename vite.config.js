6d659b11ed95508ce1c5fae6730978443d579a12
Abha Mam <apm6283@psu.edu>
2025-04-18 01:40:49 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',       // make sure it listens on all interfaces
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: [
+      'weather-dashboard-jqbp.onrender.com',  // Render public host
+    ]
+  }
 })
